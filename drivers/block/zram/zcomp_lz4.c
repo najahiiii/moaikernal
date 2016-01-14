748c9b85e54f5f9feb35a8cb43ee8f025e6cfe00
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2016-01-14 15:22:35 -0800

    UPSTREAM: zram/zcomp: do not zero out zcomp private pages
    (cherry picked from commit e02d238c9852a91b30da9ea32ce36d1416cdc683)
    
    Do not __GFP_ZERO allocated zcomp ->private pages.  We keep allocated
    streams around and use them for read/write requests, so we supply a
    zeroed out ->private to compression algorithm as a scratch buffer only
    once -- the first time we use that stream.  For the rest of IO requests
    served by this stream ->private usually contains some temporarily data
    from the previous requests.
    
    Change-Id: I7c8c123d82975b4bcf41c628e3df7e2dfc39588b
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zcomp_lz4.c
+++ drivers/block/zram/zcomp_lz4.c
@@ -22,7 +22,7 @@ static void *zcomp_lz4_create(gfp_t flags)
 	ret = kmalloc(LZ4_MEM_COMPRESS, flags);
 	if (!ret)
 		ret = __vmalloc(LZ4_MEM_COMPRESS,
-				flags | __GFP_ZERO | __GFP_HIGHMEM,
+				flags | __GFP_HIGHMEM,
 				PAGE_KERNEL);
 	return ret;
 }
