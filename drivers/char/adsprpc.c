86ad8058fb07ba062c98dfc258626d0f9e5e7e72
Tharun Kumar Merugu <mtharu@codeaurora.org>
2018-08-23 05:00:05 +0530

    msm: adsprpc: allocate all remote memory in kernel
    Allocate all memory given to remote subsystem in the kernel
    instead of mapping memory allocated in userspace.
    
    Change-Id: I79c1f40d426e271403afa67514714fe6af26cf4e
    Acked-by: Thyagarajan Venkatanarayanan <venkatan@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>
    Signed-off-by: Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -129,7 +129,7 @@ struct fastrpc_buf {
 	struct fastrpc_file *fl;
 	void *virt;
 	uint64_t phys;
-	size_t size;
+	ssize_t size;
 	struct dma_attrs attrs;
 	uintptr_t raddr;
 	uint32_t flags;
@@ -427,7 +427,7 @@ static int fastrpc_mmap_find(struct fastrpc_file *fl, int fd, uintptr_t va,
 	return -ENOTTY;
 }
 
-static int dma_alloc_memory(dma_addr_t *region_start, void **vaddr, size_t size,
+static int dma_alloc_memory(dma_addr_t *region_start, void **vaddr, ssize_t size,
 			struct dma_attrs *attrs)
 {
 	struct fastrpc_apps *me = &gfa;
@@ -663,7 +663,7 @@ bail:
 	return err;
 }
 
-static int fastrpc_buf_alloc(struct fastrpc_file *fl, size_t size,
+static int fastrpc_buf_alloc(struct fastrpc_file *fl, ssize_t size,
 				struct dma_attrs attr, uint32_t rflags,
 				int remote, struct fastrpc_buf **obuf)
 {
@@ -1551,7 +1551,7 @@ static int fastrpc_init_process(struct fastrpc_file *fl,
 	int err = 0;
 	struct fastrpc_ioctl_invoke_fd ioctl;
 	struct smq_phy_page pages[1];
-	struct fastrpc_mmap *file = NULL, *mem = NULL;
+	struct fastrpc_mmap *file = 0, *mem = 0;
 	struct fastrpc_buf *imem = NULL;
 
 	if (init->flags == FASTRPC_INIT_ATTACH) {
@@ -1928,7 +1928,7 @@ static int fastrpc_internal_mmap(struct fastrpc_file *fl,
 				 struct fastrpc_ioctl_mmap *ud)
 {
 
-	struct fastrpc_mmap *map = NULL;
+	struct fastrpc_mmap *map = 0;
 	struct fastrpc_buf *rbuf = NULL;
 	uintptr_t raddr = 0;
 	int err = 0;
