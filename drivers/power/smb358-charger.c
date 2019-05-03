52be3131bc1d14c7834285380a69a75948d987e2
najahiiii <tlogitechnjhi@gmail.com>
2019-05-03 23:16:33 +0700

    power: smb358: hardcode current to 1.5mA
    * I've edited max current limit of Quick Charge to 1.8mA
    * but mostly xiaomi phone shiped with 1.5mA output current
    * so, in this changes will re-write max current prop on Pie roms to 1.5mA.
    
    Signed-off-by: najahiiii <tlogitechnjhi@gmail.com>

--- drivers/power/smb358-charger.c
+++ drivers/power/smb358-charger.c
@@ -1244,7 +1244,7 @@ static int get_prop_current_now(struct smb358_charger *chip)
 		} else {
 			pr_debug("No BMS supply registered return 0\n");
 		}
-	return 1000;
+	return 1500;
 }
 
 static int smb358_get_prop_charge_type(struct smb358_charger *chip)
